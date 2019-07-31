9834de8e7b6fc34e748dbd4e4e06c02164bf5c6a
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-31 17:53:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,10 @@
 class School
   def initialize(name)
     @name = name
+    @roster = {}
   end
   def roster
-    @roster = {}
+    @roster
   end
   def add_student(student_name, student_grade)
     if @roster.has_key?(student_grade)
