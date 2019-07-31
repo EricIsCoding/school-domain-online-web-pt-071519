9cdaa62e9a5f2c821803d8cc6cc71bf410d28b21
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-31 17:52:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,11 @@ class School
     @roster = {}
   end
   def add_student(student_name, student_grade)
-    if @roster
+    if @roster.has_key?(student_grade)
+      @roster[student_grade] << student_name
+    else
+      @roster[student_grade] = []
+      @roster[student_grade] << student_name
+    end
   end
 end
